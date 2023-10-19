b709cc49021d7b645c566d3993d7d198c9e157a6
Stelios D <snd14641@gmail.com>
2023-10-19 09:54:20 -0400

    More practice for cs137 exam.

--- main.c
+++ main.c
@@ -3,16 +3,35 @@
 #include <assert.h>
 extern void score_update();
 
-int max_input(int largest){
-    int n = 0;
-    if(!scanf("%d", &n)){
-        return largest;
+int read_ints(){
+    int min,min_i,max,max_i,x,index;
+    if(scanf("%d", &x)){
+        min = x;
+        max = x;
+        index = 1;
+        max_i = 1;
+        min_i = 1;
+    } else {
+        printf("No integer given.");
+        return 0;
     }
-    largest = (largest>n)?largest:n;
-    max_input(largest);
+
+    while(scanf("%d", &x)){
+        index++;
+        if(x>=max){
+            max = x;
+            max_i = index;
+        }
+        if(x<=min){
+            min = x;
+            min_i = index;
+        }
+    }
+    printf("(%d, %d)(%d, %d)", min, min_i, max, max_i);
+    return 0;
 }
 
 
 int main(void) {
-    printf("%d", max_input(0));
+    read_ints();
 }
--- practice/midterm_practice.c
+++ practice/midterm_practice.c
@@ -54,3 +54,32 @@ int max_input(int largest){
     largest = (largest>n)?largest:n;
     max_input(largest);
 }
+
+int read_ints(){
+    int min,min_i,max,max_i,x,index;
+    if(scanf("%d", &x)){
+        min = x;
+        max = x;
+        index = 1;
+        max_i = 1;
+        min_i = 1;
+    } else {
+        printf("No integer given.");
+        return 0;
+    }
+
+    while(scanf("%d", &x)){
+        index++;
+        if(x>=max){
+            max = x;
+            max_i = index;
+        }
+        if(x<=min){
+            min = x;
+            min_i = index;
+        }
+    }
+    printf("(%d, %d)(%d, %d)", min, min_i, max, max_i);
+    return 0;
+}
+
