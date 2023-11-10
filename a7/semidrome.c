b46cc7c504deb70ae6224cb889db36c31b4a34a2
Stelios D <snd14641@gmail.com>
2023-11-10 16:20:55 -0500

    Started a7

---
+++ a7/semidrome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool is_semidrome (char *s) {
+
+
+
+    return true;
+}
+
--- main.c
+++ main.c
@@ -1,38 +1,13 @@
-#include "a6/vlinteger.h"
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#include "a7/semidrome.c"
 
 int main(void){
-
-    struct linteger *int1 = vlintegerCreate();
-    printf("Enter the digits separated by a space: \n");
-    vlintegerRead(int1);
-    vlintegerPrint(int1);
-    assert(int1->arr[0] !=0);
-
-    char c;
-    scanf("%c", &c);
-    struct linteger *int2 = vlintegerCreate();
-    printf("Enter the digits separated by a space: \n");
-    vlintegerRead(int2);
-    vlintegerPrint(int2);
-    assert(int2->arr[0] !=0);
-
-    scanf("%c", &c);
-    struct linteger *add = vlintegerAdd(int1, int2);
-    printf("addition\n");
-    vlintegerPrint(add);
-    assert(add->arr[0] !=0);
-
-    scanf("%c", &c);
-    struct linteger *mult = vlintegerMult(int1, int2);
-    printf("multiplication\n");
-    vlintegerPrint(mult);
-    assert(mult->arr[0] !=0);
-
-    vlintegerDestroy(int1);
-    vlintegerDestroy(int2);
-    vlintegerDestroy(add);
-    vlintegerDestroy(mult);
-
+    assert(is_semidrome("popeye"));
+    assert(is_semidrome("racecar"));
+    assert(!is_semidrome("aab"));
+    assert(!is_semidrome(""));
+    return 0;
 }
